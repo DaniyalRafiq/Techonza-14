660ad8b95f152db53d08f7af7929c42a0a386c8d
bb420f@gmail.com <bb420f@gmail.com>
2023-12-16 17:52:51 +0500

    hogya hn done

--- js/script.js
+++ js/script.js
@@ -131,8 +131,12 @@ $(document).ready(function () {
       prevEl: ".swiper_hero_coupon_btn_main > .swiper-button-prev",
     },
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       clickable: true,
+    },
+    autoplay: {
+      delay: 3000, 
+      disableOnInteraction: false,
     },
   });
 });
